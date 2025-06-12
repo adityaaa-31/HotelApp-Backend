21474ddbd2f0c2b3a0ff1982420a421592cb31b6
aditya shedge <adityavshedge3101@gmail.com>
2025-06-12 12:11:27 +0530

    add findByEmail in user repo

--- src/main/java/com/example/restapp/repository/UserRepository.java
+++ src/main/java/com/example/restapp/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.example.restapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.restapp.model.User;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    Optional<User> findByEmail(String email);
 }
