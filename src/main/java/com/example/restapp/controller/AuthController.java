bd0db3750cb8608b5c15b2136f645a218fcf88c8
aditya shedge <adityavshedge3101@gmail.com>
2025-06-17 11:58:33 +0530

    update auth controller to use request dto

--- src/main/java/com/example/restapp/controller/AuthController.java
+++ src/main/java/com/example/restapp/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.example.restapp.controller;
 
+import com.example.restapp.dto.SignUpRequestDTO;
+import com.example.restapp.model.User;
 import com.example.restapp.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.restapp.model.User;
 import com.example.restapp.service.UserService;
 
 import java.util.Optional;
@@ -16,18 +18,26 @@ import java.util.Optional;
 public class AuthController {
 
     private UserService userService;
-
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthController(UserService userService, UserRepository userRepository) {
+
+    public AuthController(UserService userService, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody SignUpRequestDTO signUpRequest) {
 
         try {
+            User user = User.builder()
+                    .email(signUpRequest.getEmail())
+                    .name(signUpRequest.getUsername())
+                    .password(passwordEncoder.encode(signUpRequest.getPassword()))
+                    .build();
+
             Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
 
             if(existingUser.isPresent()) {
