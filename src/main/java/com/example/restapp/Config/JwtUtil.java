df886f6d65788f1c6526606bea28db95b9972ab5
aditya shedge <adityavshedge3101@gmail.com>
2025-06-17 14:35:28 +0530

    create jwt util with generate token and validate methods

---
+++ src/main/java/com/example/restapp/Config/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.example.restapp.Config;
+
+import com.example.restapp.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+import java.security.Key;
+
+public class JwtUtil {
+
+    private final Key secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getName())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getEncoded()))
+                .compact();
+    }
+
+    public boolean validateToken(String token, User user) {
+        Claims claims = extractClaims(token);
+        return (claims.getSubject().equals(user.getName()) && 
+                !claims.getExpiration().before(new Date()));
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+
+}
