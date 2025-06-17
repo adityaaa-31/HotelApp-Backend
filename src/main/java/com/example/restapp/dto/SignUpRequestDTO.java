268e74030f9a7f859788b4dd97baaa601033c4cb
aditya shedge <adityavshedge3101@gmail.com>
2025-06-17 11:57:55 +0530

    create sign-up request dto

---
+++ src/main/java/com/example/restapp/dto/SignUpRequestDTO.java
@@ -0,0 +1,20 @@
+package com.example.restapp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class SignUpRequestDTO {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
