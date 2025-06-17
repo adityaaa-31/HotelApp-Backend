26585b0ea716400d9d670bbbb4f9953b48fc78b2
aditya shedge <adityavshedge3101@gmail.com>
2025-06-17 14:36:16 +0530

    create auth filter and impl its method

---
+++ src/main/java/com/example/restapp/Config/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.example.restapp.Config;
+
+import com.example.restapp.model.User;
+import com.example.restapp.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+    private final SecretKey secretKey;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserService userService, @Value("${jwt.secret}") String secretKey) {
+        this.jwtUtil = jwtUtil;
+        this.userService = userService;
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.split(" ")[1];
+        String username = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+
+        User user = userService.loadByUsername(username);
+
+        if(jwtUtil.validateToken(token, user)){
+            UsernamePasswordAuthenticationToken authenticationToken
+                    = new UsernamePasswordAuthenticationToken(
+                            user,
+                    null,
+                        user.getAuthorities()
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
