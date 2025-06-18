dff74f4e01b06a5a0c3f821e309190186ba0ca2d
aditya shedge <adityavshedge3101@gmail.com>
2025-06-18 10:43:14 +0530

    use parametrized logger

--- src/main/java/com/example/restapp/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/restapp/config/jwt/JwtAuthenticationEntryPoint.java
@@ -15,7 +15,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        logger.error("Unauthorized error: " + authException.getMessage());
+        logger.error("Unauthorized error: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing authentication.");
     }
 }
