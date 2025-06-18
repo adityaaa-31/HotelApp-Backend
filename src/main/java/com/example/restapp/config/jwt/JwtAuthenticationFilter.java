dbedb5bee9cc91d2d238029f006e6a46aa1c1886
aditya shedge <adityavshedge3101@gmail.com>
2025-06-18 15:54:13 +0530

    add component annnotation

--- src/main/java/com/example/restapp/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/restapp/config/jwt/JwtAuthenticationFilter.java
@@ -12,11 +12,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
